d81d4493f3fbdafdbde3665237029fdcbcb8c279
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-05-13 20:37:03 +0600

    file and folder setup

---
+++ lib/app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ChatMessaging extends StatefulWidget {
+  const ChatMessaging({super.key});
+
+  @override
+  State<ChatMessaging> createState() => _ChatMessagingState();
+}
+
+class _ChatMessagingState extends State<ChatMessaging> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
